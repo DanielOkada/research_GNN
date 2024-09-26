4aac332390de14a3acc71590548457ccd0f0da4d
Daniel <s223298@stn.nagaokaut.ac.jp>
2024-09-26 12:21:48 +0900

    node2vec

--- kanagawa.py
+++ kanagawa.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel('data/明治35年神奈川.xlsx', sheet_name="明治35")
 
+# "・"を削除する(Graphvizでエラーが出る)
+df['役員名'] = df['役員名'].str.replace('・', '_', regex=False)
+
 edges_df = df[["會社名", "役員名"]].dropna()
 edges = edges_df.itertuples(index=False)
 
@@ -14,28 +17,28 @@ G.add_edges_from(edges)
 yakuin_nodes = edges_df["役員名"].unique()
 G = nx.projected_graph(G, yakuin_nodes)
 
-# nx.write_graphml(G, 'yakuin.graphml')
+nx.write_graphml(G, 'data/kanagawa_yakuin.graphml')
 
-# # 最大連結成分のみでサブグラフを作成
-# largest_cc = max(nx.connected_components(G), key=len)
-# G = G.subgraph(largest_cc)
+# 最大連結成分のみでサブグラフを作成
+largest_cc = max(nx.connected_components(G), key=len)
+G = G.subgraph(largest_cc)
 
-# pos = nx.spring_layout(G)  # spring_layoutは一例
-# # 図のサイズを設定
-# plt.figure(figsize=(8, 8))
-#
-# # ノードを描画
-# nx.draw_networkx_nodes(G, pos)
-#
-# # エッジを描画
-# nx.draw_networkx_edges(G, pos)
-#
-# # ノードにラベルを付ける
-# nx.draw_networkx_labels(G, pos, font_family="MS Gothic")
-#
-# # 表示
-# plt.axis('off')
-# plt.show()
+pos = nx.spring_layout(G)  # spring_layoutは一例
+# 図のサイズを設定
+plt.figure(figsize=(8, 8))
+
+# ノードを描画
+nx.draw_networkx_nodes(G, pos)
+
+# エッジを描画
+nx.draw_networkx_edges(G, pos)
+
+# ノードにラベルを付ける
+nx.draw_networkx_labels(G, pos, font_family="MS Gothic")
+
+# 表示
+plt.axis('off')
+plt.show()
 
 # 媒介中心性を計算
 betweenness = nx.betweenness_centrality(G)
--- test.py
+++ test.py
@@ -1,15 +1,23 @@
+import networkx as nx
 import pandas as pd
+from matplotlib import pyplot as plt
 
-# credits.csvを読み込む
-credits = pd.read_csv('data/credits.csv')
+graphml_file = 'data/kanagawa_yakuin.graphml'
 
-# castデータのみを取り出す
-cast = credits['cast']
+# GraphMLファイルの読み込み
+G = nx.read_graphml(graphml_file)
 
-# キャスト情報を正規化する
-cast = pd.DataFrame(cast.values.tolist(), index=credits.index)
+# edges_df = df[["會社名", "役員名"]].dropna()
+# edges = edges_df.itertuples(index=False)
+#
+# G = nx.Graph()
+# G.add_edges_from(edges)
+#
+# # 1部グラフ作成
+# yakuin_nodes = edges_df["役員名"].unique()
+# G = nx.projected_graph(G, yakuin_nodes)
 
-print(cast)
-# 必要な列のみを取り出す
-cast = cast[[0,1,2,3]]
-cast.columns = ['cast_id', 'character', 'credit_id', 'id']
+# 可視化
+plt.figure(figsize=(8,5)) # 適切なサイズで
+nx.draw(G, pos=nx.nx_pydot.graphviz_layout(G), with_labels=True, font_family="MS Gothic")
+plt.show()
